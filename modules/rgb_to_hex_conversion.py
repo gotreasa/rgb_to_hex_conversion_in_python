32d7d25a0e94d487859cb3129c7d729737e7ab4c
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-15 14:21:50 +0000

    refactor: programmatically calculate the hex values

--- modules/rgb_to_hex_conversion.py
+++ modules/rgb_to_hex_conversion.py
@@ -1,12 +1,15 @@
+def extract_hex_code(decimal_number: int) -> str:
+    result = hex(decimal_number).replace("0x", "").upper()
+    if len(result) == 1:
+        return f"0{result}"
+    return result
+
+
 def rgb(red: int, green: int, blue: int) -> str:
+    if not isinstance(red, int):
+        raise ValueError("❗️ Value for red should be an integer")
     if not isinstance(green, int):
         raise ValueError("❗️ Value for green should be an integer")
     if not isinstance(blue, int):
         raise ValueError("❗️ Value for blue should be an integer")
-    if red == 0 and green == 0 and blue == 0:
-        return "000000"
-    if red == 255 and green == 255 and blue == 255:
-        return "FFFFFF"
-    if red == 148 and green == 0 and blue == 211:
-        return "9400D3"
-    raise ValueError("❗️ Value for red should be an integer")
+    return "".join([extract_hex_code(red), extract_hex_code(green), extract_hex_code(blue)])
