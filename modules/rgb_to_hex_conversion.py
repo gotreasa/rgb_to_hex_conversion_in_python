c6b5a8660aa7800b9968f8d63b334cd23df31e5e
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-15 13:56:50 +0000

    feat: should error if any of the second input is not an integer

--- modules/rgb_to_hex_conversion.py
+++ modules/rgb_to_hex_conversion.py
@@ -1,2 +1,4 @@
 def rgb(red: int, green: int, blue: int) -> str:
+    if not isinstance(green, int):
+        raise ValueError("❗️ Value for green should be an integer")
     raise ValueError("❗️ Value for red should be an integer")
--- tests/unit/test_0_installer_runner.py
+++ tests/unit/test_0_installer_runner.py
@@ -7,3 +7,8 @@ def describe_rgb():
         """🧪 should error if any of the first input is not an integer"""
         with pytest.raises(ValueError, match="❗️ Value for red should be an integer"):
             rgb_to_hex_conversion.rgb("blah", 2, 2)
+
+    def should_error_when_second_input_is_not_integer():
+        """🧪 should error if any of the second input is not an integer"""
+        with pytest.raises(ValueError, match="❗️ Value for green should be an integer"):
+            rgb_to_hex_conversion.rgb(2, "blah", 2)
